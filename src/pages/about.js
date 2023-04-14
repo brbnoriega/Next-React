b814ac66c2cb3dfee1b2d4d2dc462afd63eb768d
barbara <91904482+brbnoriega@users.noreply.github.com>
2023-04-14 16:37:53 -0300

    first commit next

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,23 +4,23 @@ import styles from "../styles/Home.module.css";
 
 
 
-export default function Home() {
+export default function AboutPage() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Home - Barbara</title>
-        <meta name="description" content="Home app" />
+        <meta name="description" content="About page" />
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Ir a <a href="/about">About</a>
+          Ir a <a href="/">Home</a>
         </h1>
 
         <p className={styles.description}>
           Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>pages/about.js</code>
         </p>
       </main>
     </div>

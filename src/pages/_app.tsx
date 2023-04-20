e03774892b7fde8ea9e3a045d3e12ad0a5176bbd
barbara <91904482+brbnoriega@users.noreply.github.com>
2023-04-20 18:02:39 -0300

    _app.jsx to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ReactElement, ReactNode } from "react";
+import { NextPage } from "next";
+import { AppProps } from "next/app";
+import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-//punto central de la aplicacion
+//componente
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+//defino las props de este componente:
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout 
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  //punto central de la aplicacion
   const getLayout = Component.getLayout || ((page) => page);
   // return <Component {...pageProps} />
 
-return getLayout(<Component {...pageProps} />) //renderiza el componente
+  return getLayout(<Component {...pageProps} />); //renderiza el componente
 }
